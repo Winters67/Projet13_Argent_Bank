a0350662702ab9fff8d9611eea98aacce6337a0a
Winters67 <emmanuel.vbg@gmail.com>
2023-06-26 11:23:45 +0200

    Fist commit

---
+++ src/components/Hearder/Hearder.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Header.scss"
+import logo from '../../assets/img/argentBankLogo.png'
+
+
+const Hearder = () => {
+    return (
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="./index.html">
+                <img
+                    className="main-nav-logo-image"
+                    src={logo}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item" href="./sign-in.html">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Hearder;
